459bd136874140d3a89b9cae36192aa71087b62a
Manisha <manishaanumolu1232gmail.com>
2022-02-18 12:57:54 +0530

    Changes Make

--- ConsoleApp12/Program.cs
+++ ConsoleApp12/Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello Git!");
+for(int j=0; j<20;j++)
+{
+    Console.WriteLine("Hello Git");
+}
